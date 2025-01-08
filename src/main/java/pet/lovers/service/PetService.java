74a3017ba0783fb5fa73a2c6afe968b62af26649
Anna <annamixailidou04@gmail.com>
2025-01-08 20:38:55 +0200

    not done
    got interrupted lol, to synexizw se lg

---
+++ src/main/java/pet/lovers/service/PetService.java
@@ -0,0 +1,28 @@
+package pet.lovers.service;
+
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+import pet.lovers.entities.Pet;
+import pet.lovers.repositories.PetRepository;
+
+import java.util.List;
+
+@Service
+public class PetService {
+    private PetRepository petRepository;
+
+    public PetService(PetRepository petRepository) {
+        this.petRepository = petRepository;
+    }
+
+    @Transactional
+    public List<Pet> getPets(){
+        return petRepository.findAll();
+    }
+
+    @Transactional
+    public savePet(Pet pet){
+        petRepository.save(pet)
+    }
+
+}
