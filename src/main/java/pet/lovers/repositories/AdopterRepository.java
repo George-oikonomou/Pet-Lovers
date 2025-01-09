fd318fbf624f44745216a847d25e15bb6650f015
Anna <annamixailidou04@gmail.com>
2025-01-09 03:45:01 +0200

    unused imports, methods

--- src/main/java/pet/lovers/repositories/AdopterRepository.java
+++ src/main/java/pet/lovers/repositories/AdopterRepository.java
@@ -3,12 +3,7 @@ package pet.lovers.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pet.lovers.entities.Adopter;
-import pet.lovers.entities.AdoptionRequest;
-import pet.lovers.entities.User;
-
-import java.util.Optional;
 
 @Repository
 public interface AdopterRepository extends JpaRepository<Adopter, Integer> {
-    Optional<Adopter> findByAdoptionRequest(AdoptionRequest adoptionRequest); //may be more useful in User repository
 }
